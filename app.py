609fcf393c8bd6b30597ee40bbbf266a774cd33e
Kelly Owenya <owenya@uwindsor.ca>
2022-06-09 02:45:54 -0400

    woo

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ header = st.container()
 
 with header:
     st.title("Welcome to [App Name].")
+    st.image("https://pharmchoices.com/wp-content/uploads/2021/04/5a374dca6b5fa9.6831613015135738344398.png")
+    st.subheader("We'll help you make smart and informed decisions about your use of medication, all while keeping you safe.")
+    st.button("Get started")
